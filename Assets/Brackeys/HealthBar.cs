d08628f5580ceec3e1e4c22422e05ec1e6f34783
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2020-07-26 14:16:55 -0400

    health bar ui/slider

---
+++ Assets/Brackeys/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthSlider;
+
+    // Set Health Bar UI current health to specified value
+    public void SetHealth(int health)
+    {
+        healthSlider.value = health;
+    }
+
+    // Set Health Bar UI maximum health to specified value
+    public void SetMaxHealth(int health)
+    {
+        healthSlider.maxValue = health;
+        SetHealth(health);
+    }
+}
